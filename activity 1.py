a42c04eabc828ac36b14e40a2971906af35e93bc
karimaradysoliman-art <karimaradysoliman@gmail.com>
2026-05-01 12:46:50 +0300

    Add files via upload

---
+++ activity 1.py
@@ -0,0 +1,3 @@
+print("hello how are you")
+print(789*963)
+print("hello how are you",789*963)
---
+++ activity 2.py
@@ -0,0 +1,9 @@
+x=5
+y="hello"
+print(x)
+print(y)
+name=input("enter your name")
+print("welcome to codingal",name)
+u=67
+p=10
+print(u*p)
